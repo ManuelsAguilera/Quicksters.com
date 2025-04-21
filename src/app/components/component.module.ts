4f9a61150b067af5be503492b7ccb21c05d32c50
manuelsaguilera <maguileraespejo@gmail.com>
2025-04-21 00:03:01 -0400

    Pagina foros, y componentes de foros creados

--- src/app/components/component.module.ts
+++ src/app/components/component.module.ts
@@ -7,11 +7,11 @@ import { HeaderComponent } from '../components/header/header.component';
 import { CarouselComponent } from '../components/carousel/carousel.component';
 import { GalleryComponent } from '../components/gallery/gallery.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { ForoTitleComponent} from './foro-title/foro-title.component'; // Importa el componente ForoTitleComponent
 @NgModule({
 
-  declarations: [HeaderComponent,CarouselComponent,GalleryComponent,FooterComponent], // Declara el componente
+  declarations: [HeaderComponent,CarouselComponent,GalleryComponent,FooterComponent,ForoTitleComponent], // Declara el componente
   imports: [CommonModule,IonicModule,RouterModule], // Importa CommonModule para usar directivas básicas de Angular
-  exports: [HeaderComponent,CarouselComponent,GalleryComponent,FooterComponent] // Exporta el componente para que otros módulos puedan usarlo
+  exports: [HeaderComponent,CarouselComponent,GalleryComponent,FooterComponent,ForoTitleComponent] // Exporta el componente para que otros módulos puedan usarlo
 })
 export class ComponentModule {}
