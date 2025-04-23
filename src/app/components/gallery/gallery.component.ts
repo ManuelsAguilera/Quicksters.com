5dfd6b06116b70e2d3d418d4e8f7219fd5d6e914
manuelsaguilera <maguileraespejo@gmail.com>
2025-04-22 21:41:44 -0400

    Componente de galeria mejorado para moviles, y ajustado home

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -15,36 +15,42 @@ interface GalleryImage {
 })
 export class GalleryComponent  implements OnInit {
   //
-   @Input() rows: number = 2;
-   @Input() images: GalleryImage[] = [
-    { src: 'assets/shapes.svg', alt: 'Imagen 1', title: 'Juegos 1' },
-    { src: 'assets/shapes.svg', alt: 'Imagen 2', title: 'Juegos 2' },
-    { src: 'assets/shapes.svg', alt: 'Imagen 3', title: 'Juegos 3' },
-    { src: 'assets/shapes.svg', alt: 'Imagen 4', title: 'Juegos 4' },
-    { src: 'assets/shapes.svg', alt: 'Imagen 5', title: 'Juegos 5' },
-    { src: 'assets/shapes.svg', alt: 'Imagen 6', title: 'Juegos 6' },
-    { src: 'assets/shapes.svg', alt: 'Imagen 7', title: 'Juegos 7' },
-    { src: 'assets/shapes.svg', alt: 'Imagen 8', title: 'Juegos 8' },
-    { src: 'assets/shapes.svg', alt: 'Imagen 9', title: 'Juegos 9' },
-    { src: 'assets/shapes.svg', alt: 'Imagen 10', title: 'Juegos 10' },
-    { src: 'assets/shapes.svg', alt: 'Imagen 11', title: 'Juegos 11' },
-    { src: 'assets/shapes.svg', alt: 'Imagen 12', title: 'Juegos 12' },
-  ];
+   @Input() cantImages: number= 12;
+   @Input() title:string= "no"
+   
+  images: GalleryImage[] = this.createImagesList();
 
   //Lo que mostraremos de las imagenes
   imagesGrouped: GalleryImage[][] = [];
 
    
-  constructor() { }
+  constructor() {
+  }
 
   ngChanges() {
-    this.groupImages();
+    //this.groupImages();
   }
 
   ngOnInit() {
-    this.groupImages();
+    //this.groupImages();
   }
 
+  private createImagesList():GalleryImage[]
+  {
+    let imageList:GalleryImage[]= []
+    for (let i = 0; i < this.cantImages; i++)
+    {
+      //Crear una imagen nueva y hacer append a nuestra lista
+      imageList.push({
+        src: `assets/shapes.svg`,  // Ciclo entre 5 imágenes de juegos
+        alt: `Juego ${i} screenshot`,
+        title: `Juego ${i}`
+      });
+
+    }
+    return imageList;
+  }
+ /*
   private groupImages()
   {
     // Dividir las imágenes en filas
@@ -58,7 +64,7 @@ export class GalleryComponent  implements OnInit {
       const endIndex = Math.min(startIndex + imagesPerRow, totalImages);
       this.imagesGrouped.push(this.images.slice(startIndex, endIndex));
     }
-  }
+  }*/
 
 
 
