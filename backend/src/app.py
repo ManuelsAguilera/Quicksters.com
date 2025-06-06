6bb155063a118b873affe05534864ff3f35f5af2
manuelsaguilera <maguileraespejo@gmail.com>
2025-06-06 18:25:21 -0400

    API tiene un test

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify,request
 from flask_cors import CORS
 from config import config
 
 app = Flask(__name__)
 
-#@app.route('/')
+CORS(app)
+
+@app.route('/api/test', methods=['GET'])
+def test():
+    return {'message': 'API funcionando!'}
+
+@app.route('/api/testPost',methods=['POST'])
+def testPost():
+    data = request.get_json()
+    return jsonify({
+        'received': data,
+        'status': 'success'
+    })
+
+
 
 if __name__ == "__main__":
     app.config.from_object(config['development'])
